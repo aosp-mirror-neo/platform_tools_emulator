4195ba417fa71730924017da009bcbf2b964b3d8
Pushkar Mourya <pushkarmourya@google.com>
2025-07-31 06:10:49 +0000

    Added API 35 Playstore system image
    Added Google Play system image for API 35
    
    Bug: 426462988
    Test: Existing
    Change-Id: I35fd7bcbe550d26a8bdd0dc7e872f16080eb5dff

--- testlib/testSrc/com/android/tools/testlib/Emulator.java
+++ testlib/testSrc/com/android/tools/testlib/Emulator.java
@@ -275,8 +275,8 @@ public class Emulator implements AutoCloseable {
         // Android Automated Test Device system image
         API_33_ATD("system_image_android-33_aosp_atd_x86_64"),
         // Google Play builds
-        API_33_PlayStore("system_image_android-33PlayStore_default_x86_64");
-
+        API_33_PlayStore("system_image_android-33PlayStore_default_x86_64"),
+        API_35_PlayStore("system_image_android-35PlayStore_default_x86_64");
         /** Path to the image for this emulator {@link SystemImage}. */
         public final String path;
 
